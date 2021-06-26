200a65b2234b7c6044e0d9eeab5db20c941a8741
moshe <mosherk16@gmail.com>
2021-06-26 18:40:31 +0300

    added menu buttons + fix audio

---
+++ Assets/GamingFiles/Scripts/OptionsMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OptionsMenu : MonoBehaviour
+{
+    public Text sliderText;
+    public Slider slider;
+    public AudioSource gameMusic;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        slider.value = gameMusic.volume;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        sliderText.text = ((int)(slider.value * 100)).ToString() + "%";
+    }
+}
