bccccd042e924682b381d1d0f3f79a11e7823bfc
moshe <mosherk16@gmail.com>
2021-06-27 16:08:34 +0300

    added continue button for the main menu

---
+++ Assets/GamingFiles/Scripts/KeyboardListener.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class KeyboardListener : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            PlayerPrefs.SetInt("SavedScene", SceneManager.GetActiveScene().buildIndex);
+            SceneManager.LoadScene("Menu");
+        }
+    }
+}
--- Assets/GamingFiles/Scripts/MainMenu.cs
+++ Assets/GamingFiles/Scripts/MainMenu.cs
@@ -5,6 +5,39 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour {
 
+    private int sceneToContinue;
+
+    private void Start()
+    {
+        if (GameObject.Find("Continue") == null)
+        {
+            return;
+        }
+
+        sceneToContinue = PlayerPrefs.GetInt("SavedScene");
+        if (sceneToContinue != 0)
+        {
+            GameObject.Find("Continue").SetActive(true);
+            GameObject.Find("Play").SetActive(false);
+        } else
+        {
+            GameObject.Find("Continue").SetActive(false);
+            GameObject.Find("Play").SetActive(true);
+        }
+    }
+
+    public void ContinueGame()
+    {
+        sceneToContinue = PlayerPrefs.GetInt("SavedScene");
+        if (sceneToContinue != 0)
+        {
+            SceneManager.LoadScene(sceneToContinue);
+        } else
+        {
+            return;
+        }
+    }
+
     public void PlayGame()
     {
         SceneManager.LoadScene("Game");
