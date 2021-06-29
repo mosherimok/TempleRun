7d210bde295e6a2d2ac27cba1bb970b91f8550ed
Liron Barel <Vallejo-233@greendreamteam.onmicrosoft.com>
2021-06-29 20:55:14 +0300

    add goblin and switch when click f

--- Assets/GamingFiles/Scripts/KeyboardListener.cs
+++ Assets/GamingFiles/Scripts/KeyboardListener.cs
@@ -5,10 +5,16 @@ using UnityEngine.SceneManagement;
 
 public class KeyboardListener : MonoBehaviour
 {
+    public Avatar ethanAvatar; // A reference to the ThirdPersonCharacter on the object
+    public Avatar goblinAvatar; // A reference to the ThirdPersonCharacter on the object
+    public Transform player; // A reference to the ThirdPersonCharacter on the object
+
+    private bool isEthanEnabled = true;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        player.Find("Goblin").gameObject.SetActive(false);
     }
 
     // Update is called once per frame
@@ -18,6 +24,30 @@ public class KeyboardListener : MonoBehaviour
         {
             PlayerPrefs.SetInt("SavedScene", SceneManager.GetActiveScene().buildIndex);
             SceneManager.LoadScene("Menu");
+        }
+
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            if (isEthanEnabled)
+            {
+                player.Find("EthanBody").gameObject.SetActive(false);
+                player.Find("EthanGlasses").gameObject.SetActive(false);
+                player.Find("EthanSkeleton").gameObject.SetActive(false);
+                player.Find("Goblin").gameObject.SetActive(true);
+                player.GetComponent<Animator>().avatar = goblinAvatar;
+            }
+            else
+            {
+
+                player.Find("EthanBody").gameObject.SetActive(true);
+                player.Find("EthanGlasses").gameObject.SetActive(true);
+                player.Find("EthanSkeleton").gameObject.SetActive(true);
+                player.Find("Goblin").gameObject.SetActive(false);
+                player.GetComponent<Animator>().avatar = ethanAvatar;
+
+            }
+            isEthanEnabled = !isEthanEnabled;
+            
         }
     }
 }
